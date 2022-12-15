6d3d483f5c9bd5fe0061f1464a7631dcfbf0b1a3
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2022-12-15 07:55:48 +0100

    dev : shiny UI

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,14 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  #bslib::bs_themer()
+  output$plot_niveaux <- renderPlot({
+    random_ggplot()
+  })
+  output$plot_debits <- renderPlot({
+    random_ggplot()
+  })
+  output$data_table_bilan <- DT::renderDT({
+    random_DT(5, 5)
+  })
 }
