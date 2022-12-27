78e16b131c8196e969e8557ed0174423419cc32e
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2022-12-27 17:18:01 +0100

    dev : shiny OK

--- R/mod_vilaine_aval.R
+++ R/mod_vilaine_aval.R
@@ -45,10 +45,12 @@ mod_vilaine_aval_ui <- function(id){
                       "Débit au Pont de Cran" = "pont_de_cran_debit",
                       "Débit Vilaine estimé" = "arzal_debit_vilaine_estime",
                       "Débit Vilaine à Langon" = "langon_debit",
-                      "Débit recalculé SIVA" = "barrage_debit_recacule",
-                      "Débit total Arzal 1-5"= "debit_barQ"),
+                      "Débit recalculé SIVA *" = "barrage_debit_recacule",
+                      "Débit total Arzal 1-5 *"= "debit_barQ"),
                   inline=FALSE,
                   selected = c("pont_de_cran_debit","arzal_debit_vilaine_estime")),
+              p("* Attention plus long :  "),
+              p("  demande le chargement de 34 variables"),
               shinyWidgets::actionBttn(
                   inputId = ns("bttn_va"),
                   label = "OK",
@@ -60,7 +62,7 @@ mod_vilaine_aval_ui <- function(id){
               h3("Niveaux Vilaine"),
               shinycssloaders::withSpinner(rAmCharts::amChartsOutput(outputId = ns("va_armchart_niveaux"))),
               h3("Débits"),
-              shinycssloaders::withSpinner(rAmCharts::amChartsOutput(ns("va_armchart_debits"))),
+              shinycssloaders::withSpinner(rAmCharts::amChartsOutput(outputId = ns("va_armchart_debits"))),
            
           )
       )
@@ -98,11 +100,11 @@ mod_vilaine_aval_server <- function(id){
         }
         
         observeEvent(input$bttn_va,{
-              #browser()
+           
               #shinyCatch({
               validate(need(exists("pool"), "Il faut une connexion vers la base"))
               shinybusy::show_modal_spinner(text="chargement base") # show the modal window
-              # TODO develop load tag
+       
               va$niveaux <-
                   load_niveaux(
                       debut = as.POSIXct(strptime(input$va_datedebut-1, format = "%Y-%m-%d")),
@@ -114,16 +116,13 @@ mod_vilaine_aval_server <- function(id){
                   fin = as.POSIXct(strptime(input$va_datefin, format = "%Y-%m-%d")),
                   tags = get_tags(type="debit") ,
                   con = pool)
+             
               shinybusy::remove_modal_spinner() # remove it when done
               # TODO use attributes(niveaux)$libelle 
-#              ZoomButton <- data.frame(Unit = c("mm", "mm", 'hh', 'DD',  'MAX'), multiple = c(1, 3 ,1 ,1, 1),
-#                  label = c("10 min","30 min", "heure","jour", "MAX"))
-
-              
+             
             })
         output$va_armchart_niveaux <- renderAmCharts({
               validate(need(!is.null(va$niveaux), "cliquez sur le bouton OK pour charger des valeurs"))
-       
               rAmCharts::amTimeSeries(
                       va$niveaux, 
                       'horodate',
@@ -140,18 +139,17 @@ mod_vilaine_aval_server <- function(id){
                       linewidth = 0.2,
                       legend = TRUE           
                   ) %>%
-                  setExport(enabled = TRUE)  
+                  rAmCharts::setExport(enabled = TRUE)  
             })
         
         output$va_armchart_debits <- renderAmCharts({
               validate(need(!is.null(va$debits), "cliquez sur le bouton OK pour charger des valeurs"))
-              
-              rAmCharts::amTimeSeries(
-                      inner_join(va$debits$Q10,va$debits, 
+             rAmCharts::amTimeSeries(
+                      va$debits,
                       'horodate',
-                      colnames(va$niveaux)[2:ncol(va$niveaux)],
+                      colnames(va$debits)[2:ncol(va$debits)],
                       bullet = "round",
-                      color =   randomcoloR::distinctColorPalette(ncol(va$niveaux)-1),
+                      color =   randomcoloR::distinctColorPalette(ncol(va$debits)-1),
                       #backgroundColor = "#40555E",
                       #backgroundAlpha = 0.4,
                       bulletSize =  4,
@@ -162,7 +160,7 @@ mod_vilaine_aval_server <- function(id){
                       linewidth = 0.2,
                       legend = TRUE           
                   ) %>%
-                  setExport(enabled = TRUE)  
+                  rAmCharts::setExport(enabled = TRUE)  
             })
       })
 }
